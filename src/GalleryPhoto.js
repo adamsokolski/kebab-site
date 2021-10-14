adc19f22e8d4f1bd8fddcedbcc75bf2d79b451e6
idKrazu <idkrazu@gmail.com>
2021-10-15 00:09:31 +0200

    ✨ Add: image zoom

--- src/GalleryPhoto.js
+++ src/GalleryPhoto.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { ShimmerThumbnail } from "react-shimmer-effects";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 export default class GalleryPhoto extends React.Component {
   constructor() {
@@ -9,17 +11,19 @@ export default class GalleryPhoto extends React.Component {
 
   render() {
     return (
-      <li>
+      <>
         {this.state.loaded ? null : (
           <ShimmerThumbnail height={300} width={500} className="m-0" rounded />
         )}
-        <img
-          style={this.state.loaded ? {} : { display: "none" }}
-          src={this.props.src}
-          alt={this.props.alt}
-          onLoad={() => this.setState({ loaded: true })}
-        />
-      </li>
+        <Zoom wrapElement="li">
+          <img
+            style={this.state.loaded ? {} : { display: "none" }}
+            src={this.props.src}
+            alt={this.props.alt}
+            onLoad={() => this.setState({ loaded: true })}
+          />
+        </Zoom>
+      </>
     );
   }
 }

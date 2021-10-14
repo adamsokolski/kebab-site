8e411ac2f88d7d282fb47098d684af0a16420a6d
idKrazu <idkrazu@gmail.com>
2021-10-14 16:55:55 +0200

    🚧 image loading animation

--- src/GalleryPhoto.js
+++ src/GalleryPhoto.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
 
 export default function GalleryPhoto(props) {
   return (
     <li>
-      <img src={props.src} alt={props.alt} loading="lazy" />
+      {/* <img src={props.src} alt={props.alt} loading="lazy" /> */}
+      {<img src={props.src} alt={props.alt} loading="lazy" /> || (
+        <Skeleton height={400} width={350} />
+      )}
     </li>
   );
 }

3a33d36e86a75fccbd21fe422ca2c8861823ab7c
idKrazu <idkrazu@gmail.com>
2021-10-23 10:03:25 +0200

    🗑 Removal: photo duplicat

--- src/Gallery.js
+++ src/Gallery.js
@@ -7,7 +7,6 @@ import photo4 from "./img/4.jpg";
 import photo5 from "./img/5.jpg";
 import photo6 from "./img/6.jpg";
 import photo7 from "./img/7.jpg";
-import photo9 from "./img/9.jpg";
 import photo10 from "./img/10.jpg";
 import photo11 from "./img/11.jpg";
 import photo12 from "./img/12.jpg";
@@ -25,7 +24,6 @@ export default function Gallery() {
     [photo5, "zapiekanka z sosem czosnkowym, ketchupem oraz cebulką prażoną"],
     [photo6, "zapiekanka z sosem czosnkowym, ketchupem oraz cebulką prażoną"],
     [photo7, "zapiekanka z sosem czosnkowym, ketchupem oraz cebulką prażoną"],
-    [photo9, "zapiekanka z sosem czosnkowym, ketchupem oraz cebulką prażoną"],
     [photo10, "zapiekanka z sosem czosnkowym, ketchupem oraz cebulką prażoną"],
     [photo11, "zapiekanka z sosem czosnkowym, ketchupem oraz cebulką prażoną"],
     [photo12, "zapiekanka z sosem czosnkowym, ketchupem oraz cebulką prażoną"],

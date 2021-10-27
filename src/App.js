3f5a683966ea4e547d1dc5b8e691d3baf093c291
idKrazu <idkrazu@gmail.com>
2021-10-27 20:56:18 +0200

    ✨ Image generation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import Header from "./components/Header/Header";
-import Menu from "./components/Menu/Menu";
-import Contact from "./components/Contact/Contact";
-import Gallery from "./components/Gallery/Gallery";
+// import Header from "./components/Header/Header";
+// import Menu from "./components/Menu/Menu";
+// import Contact from "./components/Contact/Contact";
+// import Gallery from "./components/Gallery/Gallery";
+import Gimp from "./components/Gimp";
 // import Photos from "./components/Gallery/Photos";
 import "./css/fonts.css";
 
 function App() {
   return (
     <>
-      <Header />
+      <Gimp />
+      {/* <Header />
       <Gallery />
       <Menu />
-      <Contact />
+      <Contact /> */}
     </>
   );
 }
---
+++ src/components/Gimp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/Gimp.css";
+
+export default function Gimp() {
+  return (
+    <header>
+      <h1>
+        <span className="header-first-line">KEBAB POD</span>
+        <span className="header-second-line">KAUFLANDEM</span>
+        <p class="contact-number">
+          <i class="fas fa-phone-alt"></i> <span>502 432 560</span>
+        </p>
+      </h1>
+    </header>
+  );
+}

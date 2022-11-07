1dfc5629940de0a79dc24d19c919fa8b8ed6ce05
Adam Sokólski <idKrazu@gmail.com>
2022-11-07 21:37:17 +0100

    Update dodatki.js

--- src/data/dodatki.js
+++ src/data/dodatki.js
@@ -1,6 +1,6 @@
 const dodatkiArr = [
   ['Dodatkowy sos', [], ['2', '00'], 1],
-  ['Dodatkowy ser', [], ['12', '00'], 2],
+  ['Dodatkowy ser', [], ['2', '00'], 2],
   ['Dodatkowe warzywa', [], ['3', '00'], 3],
   ['Dodatkowe mięso', [], ['10', '00'], 4],
 ]
